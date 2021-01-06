recordmydesktop/recordmydesktop

commit 5ce6553ccf5502fc71d11d654c76bd1714cd36dd

*: get rid of unused frames_{total,lost}
these concepts may return but not in this form

src/rmd_load_cache.c
@@ -237,12 +237,8 @@ void *rmdLoadCache(ProgData *pdata) {
 
 			if (read_header(pdata, ifp, ucfp, frame.header)) {
 
-				if (pdata->frames_total) {
-					fprintf(stdout,	"\r[%d%%] ",
-							((frame.header->capture_frameno) * 100) / pdata->capture_frameno);
-				} else
-					fprintf(stdout,	"\r[%d frames rendered] ",
-							(frame.header->capture_frameno));
+				fprintf(stdout,	"\r[%d%%] ",
+						((frame.header->capture_frameno) * 100) / pdata->capture_frameno);
 				fflush(stdout);
 
 				if (	(frame.header->Ynum > blocknum_x * blocknum_y) ||

src/rmd_initialize_data.c
@@ -97,8 +97,6 @@ int rmdInitializeData(ProgData *pdata, EncData *enc_data, CacheData *cache_data)
 	pdata->paused			= FALSE;
 	pdata->aborted			= FALSE;
 	pdata->pause_state_changed	= FALSE;
-	pdata->frames_total		= 0;
-	pdata->frames_lost		= 0;
 	pdata->capture_frameno		= 0;
 	pdata->time_frameno		= 0;
 

src/rmd_timer.c
@@ -113,13 +113,7 @@ void *rmdTimer(ProgData *pdata) {
 			}
 		}
 
-		if (!pdata->paused) {
-			pthread_mutex_unlock(&pdata->pause_mutex);
-
-			/* FIXME TODO: detect dropped frames by delta between {time,capture}_frameno */
-			pdata->frames_total++;
-		} else
-			pthread_mutex_unlock(&pdata->pause_mutex);
+		pthread_mutex_unlock(&pdata->pause_mutex);
 
 		if (!pdata->args.nosound)
 			sync_streams(pdata, &frame_step, &delay);

src/rmd_types.h
@@ -318,11 +318,6 @@ struct _ProgData {
 	boolean aborted;			 //1 if we should abort
 	boolean pause_state_changed; //1 if pause state changed
 
-	//the following values are of no effect
-	//but they might be usefull later for profiling
-	unsigned int	frames_total,   //frames calculated by total time expirations
-			frames_lost;	//the value of shame
-
 	/* timer advances time_frameno, getframe copies time_frameno to capture_frameno
 	 * access to both is serialized by time_{mutex,cond}
 	 */

commit b3ebcb7895ec67469a67dff253dca90aba65111e

load_cache: fix divzero bug introduced by 5ce6553
--rescue mode doesn't set pdata->capture_frameno to the total

The whole --rescue initialization path is a snowflake and should
get reworked to share more in common with the normal startup.

reported by @xloem, thanks!

src/rmd_load_cache.c
@@ -217,8 +217,12 @@ void *rmdLoadCache(ProgData *pdata)
 
 			if (read_header(frame.header, icf)) {
 
-				fprintf(stdout,	"\r[%d%%] ",
-						((frame.header->capture_frameno) * 100) / pdata->capture_frameno);
+				if (pdata->capture_frameno)
+					fprintf(stdout,	"\r[%d%%] ",
+							((frame.header->capture_frameno) * 100) / pdata->capture_frameno);
+				else
+					fprintf(stdout, "\r[%d frames rendered] ",
+							frame.header->capture_frameno);
 				if (icf->chapter)
 					fprintf(stdout, "\t[Cache File %d]", icf->chapter);
 				fflush(stdout);